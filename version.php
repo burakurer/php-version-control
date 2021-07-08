51305067b4b714ed5d1f9718eb3e4441ccf2c6ef
Muhammet <58211081+r6wm@users.noreply.github.com>
2021-07-08 10:45:01 +0300

    Update version.php
    daha sağlıklı çalışan kontrol kodu eklendi

--- version.php
+++ version.php
@@ -1,5 +1,23 @@
 <?php
 
-$minPHP = '7.4.0';
+/**
+* Bu kod php sürümünüz 7.4.0 dan küçükse scripti çalıştırmayacaktır.
+*
+*/
 
-echo PHP_VERSION >= $minPHP ? "php version enough" : "php version not enough, required minimum php version " . $minPHP;
+if (version_compare(PHP_VERSION, '7.4.0', '<')) {
+    die("Sistemin stabil çalışabilmesi için minimum <b>PHP 7.4</b> versiyonu veya daha üstü gerekli! Mevcut sürüm: <b>" . PHP_VERSION . "</b>");
+}
+
+/**
+* Eğer belirlediğiniz 2 versiyon arasında çalışmasını istiyorsanız aşağıdaki kodu kullanabilirsiniz.
+* 
+* Scriptiniz sadece php 7.4.0 - 7.4.21 versiyonları arasında çalışacaktır
+* 
+*/
+
+if (version_compare(PHP_VERSION, '7.4.0', '<')) {
+    die("Sistemin stabil çalışabilmesi için <b>PHP 7.4</b> versiyonu gerekli! Mevcut sürüm: <b>" . PHP_VERSION . "</b>");
+} elseif (version_compare(PHP_VERSION, '7.4.21', '>')) {
+    die("Sistemin stabil çalışabilmesi için <b>PHP 7.4</b> versiyonu gerekli! Mevcut sürüm: <b>" . PHP_VERSION . "</b>");
+}
